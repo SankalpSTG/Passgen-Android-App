7fa79cd2c076b9a1772033d00e3ac0e7bc443552
Tejas Devda <Tejasjain803@gmail.com>
2019-09-13 13:59:46 +0530

    class edited

--- Android/PassGen/app/src/main/java/com/smartn/passgen/ScanCodeActivity.java
+++ Android/PassGen/app/src/main/java/com/smartn/passgen/ScanCodeActivity.java
@@ -9,6 +9,5 @@ public class ScanCodeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan_code);
     }
 }
